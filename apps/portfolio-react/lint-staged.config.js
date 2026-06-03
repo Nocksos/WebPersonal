54874ddbfaaa4c736ed461ad271c6174971faa08
Álvaro H.G. <nocksoslabs@gemail.com>
2026-06-03 11:59:27 +0200

    style(css): add fade-up animation and configure lint-staged properly for TASK-021-2

--- apps/portfolio-react/lint-staged.config.js
+++ apps/portfolio-react/lint-staged.config.js
@@ -1,7 +1,9 @@
 export default {
-  'src/**/*.{ts,tsx}': () => 'tsc --noEmit -p tsconfig.json',
-  'src/**/*.{ts,tsx,css,json}': [
+  'src/**/*.{ts,tsx}': [
+    () => 'tsc --noEmit -p tsconfig.json',
     'eslint --fix',
+  ],
+  'src/**/*.{ts,tsx,css,json}': [
     'prettier --write',
   ],
 }
