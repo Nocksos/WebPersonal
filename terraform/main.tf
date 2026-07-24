b0018da30eb086c876e752ba93b4caa64eb7bfc2
Álvaro H.G. <nocksoslabs@gemail.com>
2026-07-24 13:29:04 +0200

    fix: corregir sku_tier, sku_size y eliminar bloque site_config de azurerm_static_web_app

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,9 +7,6 @@ resource "azurerm_static_web_app" "main_swa" {
   name                = var.swa_name
   resource_group_name = azurerm_resource_group.web_rg.name
   location            = azurerm_resource_group.web_rg.location
-  sku                 = var.swa_sku
-
-  site_config {
-    # Configuración opcional de la aplicación
-  }
+  sku_tier            = var.swa_sku
+  sku_size            = var.swa_sku
 }
